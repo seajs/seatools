813a35b31909f8afb787a06b266e3badd9dc1162
popomore <sakura9515@gmail.com>
2013-10-14 20:01:34 +0800

    use meta.js first #5

--- tasks/meta.js
+++ tasks/meta.js
@@ -10,28 +10,37 @@ module.exports = function(grunt) {
       isPublish: false
     });
 
-    var files = find(path.resolve('./tests'))
-      .filter(function(item) {
-        return /test.html$/.test(item);
-      }).map(function(item) {
-        return item
-          .replace(path.resolve('./tests') + '/', '')
-          .replace('/test.html', '')
-          .replace(/^|$/g, '"');
-      });
+    var script = '';
+    if (fs.existsSync(path.resolve('./tests/meta.js'))) {
+      script = format(
+        '<script>%s</script>',
+        fs.readFileSync(path.resolve('./tests/meta.js')).toString()
+      );
+    } else {
+      var files = find(path.resolve('./tests'))
+        .filter(function(item) {
+          return /test.html$/.test(item);
+        }).map(function(item) {
+          return item
+            .replace(path.resolve('./tests') + '/', '')
+            .replace('/test.html', '')
+            .replace(/^|$/g, '"');
+        });
 
-    // 发布的时候会把插件的测试用例也加上
-    if (options.isPublish) {
-      var pkg = require(path.resolve('./package.json'));
-      if (!/^seajs-/.test(pkg.name) && pkg['seajs-plugin']) {
-        files = pkg['seajs-plugin'].map(function(item) {
-          return format('"/%s/tests/spec"', item);
-        }).concat(files);
+      // 发布的时候会把插件的测试用例也加上
+      if (options.isPublish) {
+        var pkg = require(path.resolve('./package.json'));
+        if (!/^seajs-/.test(pkg.name) && pkg['seajs-plugin']) {
+          files = pkg['seajs-plugin'].map(function(item) {
+            return format('"/%s/tests/spec"', item);
+          }).concat(files);
+        }
       }
+
+      script = format('<script>testSuites = [%s]</script>', files.join(','));
     }
 
     var runner = path.resolve('./_site/tests/runner.html');
-    var script = format('<script>testSuites = [%s]</script>', files.join(','));
     var html = fs.readFileSync(runner).toString()
       .replace(/(<\/head>)/, script + '$1');
     fs.writeFileSync(runner, html);
